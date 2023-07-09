62cb2b5c6444172501acef1e6c623ab26f5eb4b0
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-09 19:54:08 +0000

    finish CRUD

--- sql-crud.py
+++ sql-crud.py
@@ -32,11 +32,11 @@ Session = sessionmaker(db)
 
 session = Session()
 
-# Create dataBase using the declarative_Base subclass 'Base'
+# CREATE dataBase using the declarative_base subclass 'Base'
 
 Base.metadata.create_all(db)
 
-# Create a table of 'Ada Lovelace'
+# CREATE a table of 'Ada Lovelace'
 ada_lovelace = Programmer(
     first_name='Ada',
     last_name='Lovelace',
@@ -120,28 +120,63 @@ frank_arellano = Programmer(
 # session.add(frank_arellano)
 
 
-# Update DataBases, specific
+# UPDATE DataBases, specific
 
 # programmer_update = session.query(Programmer).filter_by(id=7).first()
 # programmer_update.famous_for = "World President"
 
-# Update DataBase, general
+# UPDATE DataBase, general
 
-people = session.query(Programmer)
-for person in people:
-    if person.gender == "F":
-        person.gender = "Female"
-    elif person.gender == "M":
-        person.gender = "Male"
-    else:
-        print('Gender undefined')
-    session.commit()
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print('Gender undefined')
+#     session.commit()
+
+
+# # DELETE from the Database
+# fname = input('User First Name: ')
+# lname = input('User Last Name: ')
+
+# programmer = session.query(Programmer).filter_by(
+#     first_name=fname, last_name=lname).first()
+
+# # Defensive programming
+
+# if programmer is not None:
+#     print('Programmer Found: ', programmer.first_name + ' ' + programmer.last_name)
+#     confirmation = input('Are you sure you want to delete data? (Y/N) ')
+#     if confirmation.lower() == 'y':
+#         session.delete(programmer)
+#         session.commit()
+#         print('Programmer has been deleted')
+#     else:
+#         print('Programmer not deleted')
+# else:
+#     print('No records found')
+
+# DELETE all
+
+programmers = session.query(Programmer)
+
+ask = input('Are you sure you want to delete all? (Y/N) ')
+if ask.lower() == 'y':
+    for programmer in programmers:
+        session.delete(programmer)
+        session.commit()
+        print(f'{programmer} was deleted!')
+else:
+    print('Database not deleted.')
 
 # commit session to the dataBase
 
 # session.commit()
 
-# query the dataBase to find our creation
+# READ the dataBase to find our creation
 
 programmers = session.query(Programmer)
 for programmer in programmers:
